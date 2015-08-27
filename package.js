19b95ac467db3954ad99ff11e9991c1fecefda8e
Ramsay Lanier <ramsay@nclud.com>
2015-08-27 15:09:01 -0400

    init

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'ramsay:react-components-core',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Core React components',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  var packages = [
+    'ramsay:react-components-nav@0.0.1',
+    'ramsay:react-components-header@0.0.1',
+    'ramsay:react-components-form@0.0.1',
+    'ramsay:react-components-page@0.0.1'
+  ];
+
+  api.use(packages, 'client');
+  api.imply(packages);
+
+});
